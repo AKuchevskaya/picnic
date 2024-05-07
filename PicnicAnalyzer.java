87f7cfbb7e21e97d2467178c7e14b88a6edb587f
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2024-05-07 14:47:59 +0300

    Добавила задание для выполнения

---
+++ PicnicAnalyzer.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PicnicAnalyzer {
+    public static void main(String[] args) {
+        
+    }
+}
