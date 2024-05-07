c974e7e50c9ac34f1e28d1d62c2ae5e3d91d1e06
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2024-05-07 15:12:43 +0300

    Написала программу

--- PicnicAnalyzer.java
+++ PicnicAnalyzer.java
@@ -6,6 +6,30 @@ import java.util.Scanner;
 
 public class PicnicAnalyzer {
     public static void main(String[] args) {
-        
+        File file = new File("input.txt");
+        Map<String, Integer> wordCount = new HashMap<>();
+        String longestWord = "";
+        try {
+            Scanner scanner = new Scanner(file);
+            int totalWords = 0;
+            while (scanner.hasNext()) {
+                String word = scanner.next().toLowerCase();
+                totalWords++;
+                if (word.length() > longestWord.length()) {
+                    longestWord = word;
+                }
+                wordCount.put(word, wordCount.getOrDefault(word, 0) +1);
+            }
+            scanner.close();
+            
+            System.out.println("Всего слов: " + totalWords);
+            System.out.println("Самое длинное слово: " + longestWord);
+            System.out.println("Частота каждого слова:");
+            for (String word : wordCount.keySet()) {
+                System.out.println(word + ": " + wordCount.get(word));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден.");
+        }
     }
 }
